3c7ebbb009807bb6500bb44118fe887864349863
codywl <codyw@protonmail.com>
2026-07-11 17:38:00 -0700

    feat: Add diffuse lighting

--- main.lua
+++ main.lua
@@ -1,16 +1,33 @@
 BACKGROUND_COLOR = { 0, 0, 0, 1 }
 Scene = {
 	spheres = {},
+	lights = {},
+	addSphere = function(c, r, color)
+		table.insert(Scene.spheres, { center = c, radius = r, color = color })
+	end,
+	addLight = function(type, intensity, position, direction)
+		if type == "ambient" then
+			table.insert(Scene.lights, { type = type, intensity = intensity })
+		end
+
+		if type == "point" then
+			table.insert(Scene.lights, { type = type, intensity = intensity, position = position })
+		end
+
+		if type == "directional" then
+			table.insert(Scene.lights, { type = type, intensity = intensity, direction = direction })
+		end
+	end,
 }
 
 love.window.setMode(1000, 1000)
-Scene.addSphere = function(c, r, color)
-	table.insert(Scene.spheres, { center = c, radius = r, color = color })
-end
 
 Scene.addSphere({ x = 0, y = -1, z = 3 }, 1, { 1, 0, 0, 1 })
 Scene.addSphere({ x = 2, y = 0, z = 4 }, 1, { 0, 0, 1, 1 })
 Scene.addSphere({ x = -2, y = 0, z = 4 }, 1, { 0, 1, 0, 1 })
+Scene.addLight("ambient", 0.2, nil, nil)
+Scene.addLight("point", 0.6, { x = 2, y = 1, z = 0 })
+Scene.addLight("directional", 0.2, nil, { x = 1, y = 4, z = 4 })
 
 Camera = {
 	position = { x = 0, y = 0, z = 0 },
@@ -42,9 +59,21 @@ function Dot(V1, V2)
 	return (V1.x * V2.x) + (V1.y * V2.y) + (V1.z * V2.z)
 end
 
+function Sub(V1, V2)
+	return { x = (V1.x - V2.x), y = (V1.y - V2.y), z = (V1.z - V2.z) }
+end
+
+function VLength(V)
+	return math.sqrt(V.x * V.x + V.y * V.y + V.z * V.z)
+end
+
 function IntersectRaySphere(origin, direction, sphere)
 	local r = sphere.radius
-	local CO = { x = origin.x - sphere.center.x, y = origin.y - sphere.center.y, z = origin.z - sphere.center.z }
+	local CO = {
+		x = origin.x - sphere.center.x,
+		y = origin.y - sphere.center.y,
+		z = origin.z - sphere.center.z,
+	}
 
 	local a = Dot(direction, direction)
 	local b = 2 * Dot(CO, direction)
@@ -81,7 +110,46 @@ function TraceRay(origin, direction, t_min, t_max)
 		return BACKGROUND_COLOR
 	end
 
-	return closest_sphere.color
+	local position = {
+		x = closest_t * direction.x,
+		y = closest_t * direction.y,
+		z = closest_t * direction.z,
+	}
+	position = { x = position.x + origin.x, y = position.y + origin.y, z = position.z + origin.z }
+	local normal = {
+		x = position.x - closest_sphere.center.x,
+		y = position.y - closest_sphere.center.y,
+		z = position.z - closest_sphere.center.z,
+	}
+	local lighting = ComputeLighting(position, normal)
+	return {
+		closest_sphere.color[1] * lighting,
+		closest_sphere.color[2] * lighting,
+		closest_sphere.color[3] * lighting,
+		1,
+	}
+end
+
+function ComputeLighting(position, normal)
+	local i = 0.0
+	local light_position = nil
+	for _, light in ipairs(Scene.lights) do
+		if light.type == "ambient" then
+			i = i + light.intensity
+		else
+			if light.type == "point" then
+				light_position = Sub(light.position, position)
+			else
+				light_position = light.direction
+			end
+
+			local n_dot_1 = Dot(normal, light_position)
+			if n_dot_1 > 0 then
+				i = i + light.intensity * n_dot_1 / (VLength(normal) * VLength(light_position))
+			end
+		end
+	end
+	return i
 end
 
 function love.draw()
